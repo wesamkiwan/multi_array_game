e31363c3c50086349e238fbadf3c0ea294b52828
wesamkiwan <wesamkiwan@github.com>
2019-08-19 14:29:01 +0200

    recommit the first commit

---
+++ multidimensional-array/src/multidimensional_array/guessing.java
@@ -0,0 +1,23 @@
+package multidimensional_array;
+
+import java.util.Scanner;
+
+public class guessing {
+
+	static Scanner test1=new Scanner(System.in);
+	public static void test(int num1, int num2) {
+		boolean accepted=false;
+
+		while (accepted==false) {
+			if (num1>num2) {
+				System.out.println("you should give an integer number between 0 and "+num2);
+				System.out.println("give a new value for please:");
+				num1=test1.nextInt();
+			}
+			else {
+				accepted=true;
+			}
+		}
+		
+		}
+}
